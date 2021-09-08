d1e317cdfc969dfc3d00646d3ad899ab5d69704e
hoho4190 <lhw14543@gmail.com>
2021-09-09 00:03:20 +0900

    Initial commit

---
+++ src/main/kotlin/com/hoho/example/kotlinjpa2/KotlinJpa2Application.kt
@@ -0,0 +1,11 @@
+package com.hoho.example.kotlinjpa2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinJpa2Application
+
+fun main(args: Array<String>) {
+	runApplication<KotlinJpa2Application>(*args)
+}
---
+++ src/test/kotlin/com/hoho/example/kotlinjpa2/KotlinJpa2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hoho.example.kotlinjpa2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJpa2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
